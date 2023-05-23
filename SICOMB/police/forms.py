7131573c19b01f0efa3df5b47485774dd7cd9941
Wallas Oliveira Almeida <wallasalmeida607@gmail.com>
2023-05-23 00:38:07 -0300

    att-23-05-2023/reconfigurando telas carga e police

--- SICOMB/police/forms.py
+++ SICOMB/police/forms.py
@@ -16,7 +16,7 @@ class PoliceForm(forms.ModelForm):
     nome = forms.CharField(widget=forms.TextInput(attrs={'class':'input-data'}), label='Nome')
     sobrenome = forms.CharField(widget=forms.TextInput(attrs={'class':'input-data'}), label='Sobrenome')
     matricula = forms.CharField(widget=forms.TextInput(attrs={'class':'input-data'}), label='Matrícula')
-    posto = forms.CharField(widget=forms.TextInput(attrs={'class':'input-data'}), label='posto')
+    posto = forms.CharField(widget=forms.TextInput(attrs={'class':'input-data'}), label='Posto')
     email = forms.CharField(widget=forms.EmailInput(attrs={'class':'input-data'}), label='Email')
     telefone = forms.CharField(widget=forms.TextInput(attrs={'class':'input-data'}), label='Telefone')
     lotacao = forms.CharField(widget=forms.TextInput(attrs={'class':'input-data'}), label='Lotação')
